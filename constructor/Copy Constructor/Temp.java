c6adb9568b0aa501cd35cae40bf5f8b49a94bef5
Bhavneesh Goyal <sakshisharma7269@gmail.com>
2026-09-07 21:30:50 +0530

    constructor program added

---
+++ constructor/Copy Constructor/Temp.java
@@ -0,0 +1,23 @@
+class Temp{
+ int x;
+ int y;
+ Temp(int x, int y){
+    this.x=x;
+    this.y=y;
+ }
+ void show(){
+   System.out.println(x);
+   System.out.println(y);
+ }
+ //Parameterized Constructor(copy constructor)
+ Temp(Temp z){
+   this.x=z.x;
+   this.y=z.y;
+ }
+ public static void main(String[] args){
+    Temp t1 = new Temp(10,20);
+    t1.show();
+    Temp t2 = new Temp(t1);
+    t2.show();
+ }
+}
